f94a5f8d6ddc0cd1ad50c71db0113ac1a06793d3
DigitalCommunity <ant1freezeca@gmail.com>
2014-11-12 11:57:30 +0300

    upload

---
+++ src/Message/AuthorizeRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Omnipay\YandexMoney\Message;
+
+use Omnipay\Common\Exception\InvalidResponseException;
+
+/**
+ * Yandexmoney Autorize Request
+ */
+class AuthorizeRequest extends PurchaseRequest
+{
+    public function getData()
+    {
+		$this->validate('action', 'orderSumAmount', 'orderSumCurrencyPaycash', 'orderSumBankPaycash', 
+						'shopid', 'invoiceId', 'customerNumber', 'password');
+
+		$data = array();
+		$data['action'] = $this->getAction();
+		if ($this->checkSign()) {
+			$data['code'] = 0;
+		}else {
+			$data['code'] = 1;
+		}
+		$data['shopId'] = $this->getShopId();
+		$data['invoiceId'] = $this->getInvoiceId();
+		$data['orderNumber'] = $this->getOrderNumber();
+		
+        return $data;
+    }
+
+	public function checkSign()
+	{
+		$string = $this->getAction() . ';' 
+				. $this->getOrderSumAmount() . ';' 
+				. $this->getOrderSumCurrencyPaycash() . ';' 
+				. $this->getOrderSumBankPaycash() . ';' 
+				. $this->getShopId() . ';' 
+				. $this->getInvoiceId() . ';' 
+				. $this->getCustomerNumber() . ';' 
+				. $this->getPassword();
+		$md5 = strtoupper(md5($string));
+		return ($md5 == $this->getMd5());
+	}
+
+    public function sendData($data)
+    {
+        return $this->response = new AuthorizeResponse($this, $data);
+    }
+}
